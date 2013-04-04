621b55433c38e88b2061cdf97e94bb187fed8539
Russell Toris <rctoris@wpi.edu>
2013-04-04 12:26:58 -0700

    function added to Stage to convert from Stage coordinates to ROS coordinates

--- src/Ros2D.js
+++ src/Ros2D.js
@@ -5,3 +5,14 @@
 var ROS2D = ROS2D || {
   REVISION : '1'
 };
+
+// convert the given global Stage coordinates to ROS coordinates
+createjs.Stage.prototype.globalToRos = function(x, y) {
+  var rosX = x / this.scaleX;
+  // change Y direction
+  var rosY = this.y - (y / this.scaleY);
+  return {
+    x : rosX,
+    y : rosY
+  };
+};
