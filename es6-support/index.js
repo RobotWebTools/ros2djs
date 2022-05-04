b6b48e89065e4671499ee788c93de6e54551623b
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-05-04 14:32:33 +0200

    Use ros3djs transpiler

---
+++ es6-support/index.js
@@ -0,0 +1,21 @@
+
+export * from './Ros2D'
+
+export * from './maps/ImageMap'
+export * from './maps/ImageMapClient'
+export * from './maps/OccupancyGrid'
+export * from './maps/OccupancyGridClient'
+export * from './maps/OccupancyGridSrvClient'
+
+export * from './models/ArrowShape'
+export * from './models/Grid'
+export * from './models/NavigationArrow'
+export * from './models/NavigationImage'
+export * from './models/PathShape'
+export * from './models/PolygonMarker'
+export * from './models/TraceShape'
+
+export * from './visualization/PanView'
+export * from './visualization/Viewer'
+export * from './visualization/ZoomView'
+
